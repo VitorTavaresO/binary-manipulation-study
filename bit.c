43d3d7f6eed8389b3e317e8cb792cfe96fca7eb9
VitorTavaresO <vitortavares.o55@gmail.com>
2023-07-17 11:17:48 -0300

    Bit Field

--- bit.c
+++ bit.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 struct port
 {
-    int bit0 : 1;
-    int bit1 : 1;
-    int bit2 : 1;
-    int bit3 : 1;
-    int bit4 : 1;
-    int bit5 : 1;
-    int bit6 : 1;
-    int bit7 : 1;
+    unsigned int bit0 : 1;
+    unsigned int bit1 : 1;
+    unsigned int bit2 : 1;
+    unsigned int bit3 : 1;
+    unsigned int bit4 : 1;
+    unsigned int bit5 : 1;
+    unsigned int bit6 : 1;
+    unsigned int bit7 : 1;
 };
 
 int main(void)
@@ -17,18 +17,12 @@ int main(void)
     p.bit0 = 1;
     p.bit1 = 0;
     p.bit2 = 1;
-    p.bit3 = 1;
+    p.bit3 = 0;
     p.bit4 = 1;
     p.bit5 = 0;
     p.bit6 = 1;
     p.bit7 = 0;
-    printf("%d\n", p.bit0);
-    printf("%d\n", p.bit1);
-    printf("%d\n", p.bit2);
-    printf("%d\n", p.bit3);
-    printf("%d\n", p.bit4);
-    printf("%d\n", p.bit5);
-    printf("%d\n", p.bit6);
-    printf("%d\n", p.bit7);
+
+    printf("%i%i%i%i%i%i%i%i", p.bit0, p.bit1, p.bit2, p.bit3, p.bit4, p.bit5, p.bit6, p.bit7);
     return 0;
 }
